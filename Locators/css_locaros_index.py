2d4fa56a97b2c8d50d73fb17900733efd484c3e8
Olga Brun <brunolga2026@gmail.com>
2026-08-11 17:10:18 +0300

    CSS_attribute

--- Locators/css_locaros_index.py
+++ Locators/css_locaros_index.py
@@ -16,24 +16,26 @@ driver = webdriver.Chrome()
 try:
     driver.get(page_url)
 
-    # input("Press Enter to close the browser...")
 
-# by tag
+# by tag_name
     button = driver.find_element(By.TAG_NAME, "button")
     button_1 = driver.find_element(By.CSS_SELECTOR, "button")
+
     print(button.tag_name)
     print(button.text)
 
-    links = driver.find_elements(By.TAG_NAME, "a")
-    for link in links:
-        print(link.text)
+    print(button_1.tag_name)
+    print(button_1.text)
 
     links = driver.find_elements(By.TAG_NAME, "a")
-    links_1 = driver.find_element(By.CSS_SELECTOR,"a") # by tag name и by css selector почти одно и то же
+    links_1 = driver.find_elements(By.CSS_SELECTOR,"a") # by tag name и by css selector почти одно и то же
     print(len(links))
     for link in links:
         print(link.text)
 
+    print(len(links_1))
+    for link in links_1:
+        print(link.text)
 
 # by class
     container = driver.find_element(By.CLASS_NAME,"container") # БЕЗ ТОЧКИ ЭТО
@@ -46,6 +48,36 @@ try:
 
     print("NAV id: ", nav.tag_name)
     print("NAV id: ", nav_1.tag_name)
+
+#by attribute
+
+    name_input = driver.find_element(By.CSS_SELECTOR, "[placeholder='Type your name']")
+    item_2 = driver.find_element(By.CSS_SELECTOR, "[href='#item2']")
+    nav_2 = driver.find_element(By.CSS_SELECTOR, "[id = 'nav']")
+
+    input_name = driver.find_element(By.CSS_SELECTOR, "[name = 'name']")
+    input_name_1 = driver.find_element(By.NAME, "name")
+
+    input = driver.find_element(By.CSS_SELECTOR, "[placeholder = 'Type your name']")
+    # поиск элемента по атрибуту по началу значения атрибута (placeholder) ставим ^=
+    starts_input = driver.find_element(By.CSS_SELECTOR, "[placeholder ^= 'Type']")
+    # поиск элемента по атрибуту по окончанию значения атрибута (placeholder) ставим $=
+    ends_input = driver.find_element(By.CSS_SELECTOR, "[placeholder $= 'name']")
+    # поиск элемента по атрибуту по любому содержимому значения атрибута (placeholder) ставим *=
+    contains_input = driver.find_element(By.CSS_SELECTOR, "[placeholder *= 'your']")
+
+# устаревший поиск linkText & partialLinkText
+
+    item1 = driver.find_element(By.LINK_TEXT, "Item 1")
+    all_items = driver.find_elements(By.PARTIAL_LINK_TEXT, "Item")
+
+    first_child = driver.find_element(By.CSS_SELECTOR, "li:first-child")
+    last_child = driver.find_element(By.CSS_SELECTOR, "li:last-child")
+    nth_child = driver.find_element(By.CSS_SELECTOR, "li:nth-child(2)")
+
+# Canada
+    canada = driver.find_element(By.CSS_SELECTOR, "tr:nth-child(3)>td:last-child")
+    assert canada.text == "Canada"
 
 finally:
     driver.quit()
